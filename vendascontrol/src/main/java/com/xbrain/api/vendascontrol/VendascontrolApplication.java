d624702d666b78a305b39045e8f9524c9b252edc
Davi Santana <davisantana.ti@gmail.com>
2023-05-15 12:35:48 -0300

    Primeiro commit! criacao do projeto springboot

---
+++ vendascontrol/src/main/java/com/xbrain/api/vendascontrol/VendascontrolApplication.java
@@ -0,0 +1,13 @@
+package com.xbrain.api.vendascontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendascontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendascontrolApplication.class, args);
+	}
+
+}
---
+++ vendascontrol/src/test/java/com/xbrain/api/vendascontrol/VendascontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbrain.api.vendascontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendascontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

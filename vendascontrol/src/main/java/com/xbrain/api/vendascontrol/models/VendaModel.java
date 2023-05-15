c9860d30ceef6fa105a22c42a29ace7e1c192cf7
Davi Santana <davisantana.ti@gmail.com>
2023-05-15 12:59:13 -0300

    Criacao das classes entidade (models)

---
+++ vendascontrol/src/main/java/com/xbrain/api/vendascontrol/models/VendaModel.java
@@ -0,0 +1,37 @@
+package com.xbrain.api.vendascontrol.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_venda")
+@Getter
+@Setter
+public class VendaModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private double valor;
+
+    @Column(nullable = false)
+    private LocalDateTime data;
+
+    @OneToOne
+    @JoinColumn(name = "id_vendedor", referencedColumnName = "id")
+    private VendedorModel vendedor;
+
+}
